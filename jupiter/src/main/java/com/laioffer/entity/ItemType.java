209eb90c277d56b5b410716f364eda2f85cb54e0
Penny Lan <peiyulan@usc.edu>
2021-08-17 22:37:34 +0800

    Twitch project created

---
+++ jupiter/src/main/java/com/laioffer/entity/ItemType.java
@@ -0,0 +1,5 @@
+package com.laioffer.entity;
+
+public enum ItemType {
+    STREAM, VIDEO, CLIP
+}
---
+++ jupiter/src/main/java/com/laioffer/external/TwitchException.java
@@ -0,0 +1,6 @@
+package com.laioffer.external;
+
+public class TwitchException extends RuntimeException{
+    public TwitchException(String errorMessage) {
+        super(errorMessage); }
+}
